6946db2ebb7b07acc3ebd616093cc49c3097d0b0
Elijah Daniel <thachromatone@gmail.com>
2022-04-21 17:35:49 +0100

    proper comment

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
-- script creates users with
-- id, email and name
+--script creates users with
+-- id, email and name
 
 CREATE TABLE IF NOT EXISTS users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 email VARCHAR(55) NOT NULL UNIQUE, name VARCHAR(255));
