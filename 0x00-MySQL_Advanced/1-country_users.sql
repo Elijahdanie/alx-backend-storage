af116181c804ee9b3332ef3ae2d4e36c53a27c08
Elijah Daniel <thachromatone@gmail.com>
2022-04-22 12:12:43 +0100

    updates

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Creates a table users with id email name and country
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
