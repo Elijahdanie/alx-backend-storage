aea202b9afeac4d4d1bb104d1c27a9a8e06f5bf6
Elijahdanie <elijahdanie@gmail.com>
2022-04-22 17:27:06 +0100

    ...

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- This selects the bands with style Glam_rock
 -- ranked by their lifespan
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-WHERE style like '%Glam rock%'
+SELECT band_name, COALESCE(split, 2020) - formed AS lifespan
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
