332a9b6136d7cdfe7151b9b2cc47a064ae6bd2b5
Elijah Daniel <thachromatone@gmail.com>
2022-04-23 15:49:36 +0100

    updated

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,12 +2,11 @@
 DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id, IN project_name, IN score)
 BEGIN
-    INSERT INTO projects VALUES(name)
-    SELECT * FROM DUAL
-    WHERE IF NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
+    INSERT INTO projects (name)
+    SELECT project_name from DUAL 
+    WHERE NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
 
     INSERT INTO corrections (user_id, project_id, score)
     VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
 END //
-
-DELIMITER;
+DELIMITER ;
