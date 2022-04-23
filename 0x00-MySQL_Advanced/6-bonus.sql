500de74c572d5e736fa30ce08601c640c795b869
Elijah Daniel <thachromatone@gmail.com>
2022-04-23 15:48:36 +0100

    changes

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,13 +1,13 @@
 -- Adds new correction to the student
-DELIMITER \\
+DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id, IN project_name, IN score)
 BEGIN
     INSERT INTO projects VALUES(name)
-    SELECT * FROM projects WHERE name=project_name
-    WHERE IF NOT EXISTS (SELECT * FROM projects WHERE name=project_name) LIMIT 1;
+    SELECT * FROM DUAL
+    WHERE IF NOT EXISTS (SELECT * FROM projects WHERE name=project_name LIMIT 1);
 
     INSERT INTO corrections (user_id, project_id, score)
     VALUES(user_id, (SELECT id from projects WHERE name=project_name), score);
-END
+END //
 
-DELIMITER
+DELIMITER;
