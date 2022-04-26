fff8f15de81b888e42cc8efba6c862b55975579b
Elijahdanie <elijahdanie@gmail.com>
2022-04-26 21:53:26 +0100

    updating work on number 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    This functions returns a list of
+    all documents in the collection mongo_collection
+    with the topic specified in the arguments
+    """
+    results = mongo_collection.find({'topics': topic})
+    return [] if results.count() == 0 else results
---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
