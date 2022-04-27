12809935caa94e3b55e9abd5a381dce99bed19d6
Elijah Daniel <thachromatone@gmail.com>
2022-04-27 13:37:47 +0100

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,22 +5,23 @@ documents in mongo db
 """
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
     """
     Fetches and format status, HTTP Request
     and logs information in the logs collection
     """
     client = MongoClient()
-    db = client["logs"]
+    collection = client.logs.nginx
     http_request = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    total_logs = db.nginx.count_documents({})
+    total_logs = collection.count()
     print('{} logs'.format(total_logs))
     print('Methods:')
     for req in http_request:
-        count = db.nginx.count_documents({"method": req})
+        count = collection.count()({"method": req})
         print('\tmethod {}: {}'.format(req, count))
 
-    stats = db.nginx.count_documents(
+    stats = collection.count(
         {"method": "GET", "path": "/status"}
         )
     print('{} status check'.format(stats))
